b8b5d3734071d2d043ab04e065d972a88377cd34
Gianluca Recchia <g.recchia@desotech.it>
2020-11-24 15:55:13 +0100

    Implement image handling

--- server/server.go
+++ server/server.go
@@ -1,9 +1,15 @@
 package server
 
 import (
+	"desotech/whoami/view"
 	"fmt"
+	"io"
 	"log"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
 )
 
 func unimplementedHandler(w http.ResponseWriter, r *http.Request) {
@@ -36,23 +42,41 @@ func metricsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func goldieHandler(w http.ResponseWriter, r *http.Request) {
-	unimplementedHandler(w, r)
+	view.WriteImageView(w, "goldie.png")
 }
 
 func zeeHandler(w http.ResponseWriter, r *http.Request) {
-	unimplementedHandler(w, r)
+	view.WriteImageView(w, "zee.png")
 }
 
 func captainkubeHandler(w http.ResponseWriter, r *http.Request) {
-	unimplementedHandler(w, r)
+	view.WriteImageView(w, "captain-kube.png")
 }
 
 func phippyHandler(w http.ResponseWriter, r *http.Request) {
-	unimplementedHandler(w, r)
+	view.WriteImageView(w, "phippy.png")
+}
+
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	basename := filepath.Base(r.URL.Path)
+	image, err := os.Open("static/images/" + basename)
+	if err != nil {
+		panic(err) // TODO: handle error more gracefully
+	}
+	defer image.Close()
+	if fileinfo, err := image.Stat(); err == nil {
+		extension := filepath.Ext(fileinfo.Name())
+		mimeType := mime.TypeByExtension(extension)
+		header := w.Header()
+		header.Set("Content-Type", mimeType)
+		header.Set("Content-Length", strconv.FormatInt(fileinfo.Size(), 10))
+	}
+	io.Copy(w, image)
 }
 
 func Start() {
 	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/images/", imageHandler)
 	http.HandleFunc("/cpustress", cpustressHandler)
 	http.HandleFunc("/memstress", memstressHandler)
 	http.HandleFunc("/health", healthHandler)
