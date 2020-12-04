cd2e077f51edd9b62813e75440209d82affdd57a
Gianluca Recchia <g.recchia@desotech.it>
2020-12-04 19:10:01 +0100

    Log CPU and memory usage periodically

--- app/log.go
+++ app/log.go
@@ -4,11 +4,20 @@ import (
 	"github.com/sirupsen/logrus"
 	"net/http"
 	"os"
+	"time"
 )
 
 func init() {
 	logrus.SetFormatter(&logrus.JSONFormatter{})
 	logrus.SetOutput(os.Stdout)
+
+	logInterval, err := time.ParseDuration(os.Getenv("WHOAMI_LOG_INTERVAL"))
+	if err != nil {
+		logInterval = 2 * time.Second
+	}
+
+	go logCPUUsage(logInterval)
+	go logMemoryUsage(logInterval)
 }
 
 func LogRequest(r *http.Request) {
@@ -17,4 +26,28 @@ func LogRequest(r *http.Request) {
 		"remote_address": r.RemoteAddr,
 		"resource":       r.RequestURI,
 	}).Infof("Handling %s request for %s from %s", r.Method, r.RequestURI, r.RemoteAddr)
+}
+
+func logCPUUsage(interval time.Duration) {
+	for {
+		timer := time.NewTimer(interval)
+		<-timer.C
+		cpuStats := CPULoad()
+		logrus.WithFields(logrus.Fields{
+			"cpu_load": cpuStats,
+		}).Info("Logging CPU load")
+	}
+}
+
+func logMemoryUsage(interval time.Duration) {
+	for {
+		timer := time.NewTimer(interval)
+		<-timer.C
+		memStats := MemInfo()
+		logrus.WithFields(logrus.Fields{
+			"used_memory":      memStats.UsedMemory,
+			"available_memory": memStats.AvailableMemory,
+			"percentage_used":  memStats.PercentageUsed,
+		}).Info("Logging memory usage")
+	}
 }
