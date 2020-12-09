41b2815f675363c37b6f2a23c6725ec3624bbae6
Gianluca Recchia <g.recchia@desotech.it>
2020-12-09 11:18:35 +0100

    Remove usages of WHOAMI_NAME
    The environment variable is no longer used.

--- app/config.go
+++ app/config.go
@@ -2,27 +2,22 @@ package app
 
 import (
 	"flag"
-	"os"
 )
 
 type Config struct {
 	Port       uint64
-	WhoamiName string
 }
 
 var port = flag.Uint64("port", 8080, "")
-var whoamiName = flag.String("name", os.Getenv("WHOAMI_NAME"), "")
 
 func init() {
 	// short version
 	flag.Uint64Var(port, "p", 8080, "")
-	flag.StringVar(whoamiName, "n", os.Getenv("WHOAMI_NAME"), "")
 }
 
 func GetConfig() Config {
 	flag.Parse()
 	return Config{
 		*port,
-		*whoamiName,
 	}
 }
