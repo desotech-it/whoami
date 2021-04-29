92683555e04bdc625e98d7952826d1a545480583
Gianluca Recchia <g.recchia@desotech.it>
2021-04-29 17:56:26 +0200

    cli: Add -version flag to print version info
    Fixes #4.

--- app/config.go
+++ app/config.go
@@ -2,6 +2,14 @@ package app
 
 import (
 	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	version string
+	commit string
+	date string
 )
 
 type Config struct {
@@ -9,11 +17,17 @@ type Config struct {
 }
 
 var port = flag.Uint64("port", 8080, "")
+var versionFlag = flag.Bool("version", false, "Print app version")
 
 func init() {
 	// short version
 	flag.Uint64Var(port, "p", 8080, "")
 	flag.Parse()
+
+	if *versionFlag {
+		fmt.Printf("whoami by Desotech %s\nGit Commit: %s\nBuilt On: %s\n", version, commit, date)
+		os.Exit(0)
+	}
 }
 
 func GetConfig() Config {
